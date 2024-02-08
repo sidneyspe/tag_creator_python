4609669f5ad95867ac3332f0f5f14aaf0a8f56b2
Sidney Pimentel <sidneyspe@gmail.com>
2024-02-08 11:50:16 -0300

    feat: view tag creator created

---
+++ src/views/tag_creator_view.py
@@ -0,0 +1,15 @@
+from src.views.http_types.http_request import HttpRequest
+from src.views.http_types.http_response import HttpResponse
+class TagCreatorView:
+
+    def validate_and_create(
+        self,
+        http_request:  HttpRequest
+    ) -> HttpResponse:
+        # body = http_request.body
+        # product_code = body["product_code"]
+        print(http_request)
+        return HttpResponse(
+            status_code=200,
+            body={"hello": "world"}
+        )
